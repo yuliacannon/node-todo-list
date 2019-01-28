38f079d85db4438c04bfd7ba04f3fb8e3210534e
yuliacannon <yuliapushka@gmail.com>
2019-01-28 18:25:41 +0200

    joi validation

--- Todo.js
+++ Todo.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Joi = require('joi');
 
-// Define collection and schema for todo item
 
 const todo = new mongoose.Schema({
   text:  String,
   done:  Boolean  
-})
+});
+
+todo.methods.JoiValidate = obj =>{
+  const schema = {
+    text: Joi.string().required().max(30),
+    done: Joi.boolean().required()
+  }
+  return Joi.validate(obj, schema);
+}
 
 module.exports = mongoose.model('Todo', todo);
